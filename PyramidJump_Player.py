e98dacc4920acb5dbf47d4b6c56dff2776ee8b16
Jonathan <nickwilde1@icloud.com>
2025-10-16 17:29:41 -0500

    Minor changes to the ai player

--- PyramidJump_Player.py
+++ PyramidJump_Player.py
@@ -1,3 +1,22 @@
 class PyramidJump_Player:
     def __init__(self) -> None:
         self.all_states = [0]*(2**15)
+        print ("start my program")
+        number = 15
+        binary_string = bin(number)[2:]
+        binary_list = list(binary_string)
+        print("the result is:")
+        print (binary_list)
+
+    def convert_number_to_state(self, number):
+        pass
+
+    def convert_state_to_number(self, state):
+        pass
+        
+# test_instace = PyramidJump_Player()
+        
+print("Testing if this works")      
+        
+my_player = PyramidJump_Player()
+
